fbc6de3c28a5d995017ef823cc0101ee60f2a409
YevaMV <eva280988@gmail.com>
2022-08-07 23:41:42 +0400

    stack reverse string

---
+++ stack/reverseString.js
@@ -0,0 +1,15 @@
+Reverse a string using a JavaScript stack
+
+function reverse(str) {
+	let stack = [];
+
+	for (let i = 0; i < str.length; i++) {
+		stack.push(str[i]);
+	}
+
+	let revStr = '';
+	while (stack.length > 0) {
+		revStr += stack.pop();
+	}
+	return revStr
+}
