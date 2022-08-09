431da7a23bf8bc702e94fd182d6c21287362aabd
YevaMV <eva280988@gmail.com>
2022-08-10 00:31:35 +0400

    reverse 3 solution

--- array/reverseString.js
+++ array/reverseString.js
@@ -3,3 +3,17 @@ function reverse(str) {
 }
 
 const reverse2 = (str) => [...str].reverse().join('');
+
+function reverse3(str) {
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'please enter a string';
+  }
+
+  const reverseArray = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseArray.push(str[i]);
+  }
+
+  console.log(reverseArray);
+  return reverseArray.join('');
+}
