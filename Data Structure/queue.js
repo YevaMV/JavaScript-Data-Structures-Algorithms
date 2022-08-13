616cfb3aaa2ea3bdfd3677cf7e72e6f636320999
YevaMV <eva280988@gmail.com>
2022-08-14 01:14:34 +0400

    data structure queue

---
+++ Data Structure/queue.js
@@ -0,0 +1,35 @@
+// first in, first out
+// enqueue => push()
+// dequeue => shift()
+
+const arr = [];
+arr.push(1);
+arr.push(2);
+arr.push(3);
+
+// arr = [1, 2, 3]
+
+arr.shift(); // 1 first in
+
+class Queue {
+  constructor() {
+    this.array = {};
+    this.head = 0;
+    this.tail = 0;
+  }
+
+  enqueue(element) {
+    this.tail++;
+    this.array[this.tail] = element;
+  }
+
+  dequeu() {
+    let removedItem = this.array[this.head];
+    delete this.array[this.head];
+    this.head++;
+    this.tail--;
+    return removedItem;
+  }
+}
+
+let queue = new Queue();
